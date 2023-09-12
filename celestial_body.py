2c2cec28aa53501dd55bc910f453d6538ee3bc71
Mees van Löben Sels <50023316+mvanlobensels@users.noreply.github.com>
2023-09-12 18:44:15 +0200

    Update celestial_body.py

--- celestial_body.py
+++ celestial_body.py
@@ -1,14 +1,16 @@
 import numpy as np
+from typing import List
 
 class CelestialBody:
 
   G = 6.6743e-11 # m3 kg-1 s-2
 
-  def __init__(self):
-    self._M = None
+  def __init__(self, name: str, mass: float, position: List[float], satellites=[]):
+    self._name = name
+    self._mass = mass
     
-    self.r = None
-    self.theta = None
+    self.r = position[0]
+    self.theta = position[1]
 
-    self.satellites = []
+    self._satellites = satellites
     
